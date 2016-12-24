6386a85ac8849729c7305e426783aa8378e3cd02
TimothyBeasley <timbeas@gmail.com>
2016-12-23 21:36:24 -0500

    moved javascript includes to end of html modified consol.log in index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ io.on('connection',function(socket){
     console.log('ok')
     socket.on('chat message',function(msg){
     io.emit('chat message',msg);
+    console.log(msg)
     });
 });
 http.listen(3000,function(){
